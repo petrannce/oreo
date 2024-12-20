7690af2913153d241e4b00f9dd8e549a11586aa0
Peter Ndirangu <pndirangu788@gmail.com>
2024-12-20 22:45:22 +0300

    updates

--- database/migrations/2024_12_20_181411_create_encounters_table.php
+++ database/migrations/2024_12_20_181411_create_encounters_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('encounters', function (Blueprint $table) {
             $table->id();
             $table->string('encounter_id')->unique();
+            $table->foreignId('appointment_id')->constrained('appointments')->onDelete('cascade');
             $table->foreignId('patient_id')->constrained('patients')->onDelete('cascade');
             $table->date('date');
             $table->time('time');
