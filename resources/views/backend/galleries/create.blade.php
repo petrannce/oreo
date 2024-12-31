6178a751b50c2876b348455d1b6fe05a953dd95f
Peter Ndirangu <pndirangu788@gmail.com>
2024-12-31 22:50:21 +0300

    updates

---
+++ resources/views/backend/galleries/create.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.backend.header')
+
+@section('content')
+
+<section class="content">
+    <div class="block-header">
+        <div class="row">
+            <div class="col-lg-7 col-md-5 col-sm-12">
+                <h2>Add Gallery
+                    <small>Welcome to Oreo</small>
+                </h2>
+            </div>
+            <div class="col-lg-5 col-md-7 col-sm-12">
+                <ul class="breadcrumb float-md-right">
+                    <li class="breadcrumb-item"><a href="index.html"><i class="zmdi zmdi-home"></i> Oreo</a></li>
+                    <li class="breadcrumb-item"><a href="{{route('galleries')}}">Galleries</a></li>
+                    <li class="breadcrumb-item active">Add</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <form action="{{route('galleries.store')}}" method="POST">
+                    @csrf
+
+                    <div class="card">
+                        <div class="header">
+                            <h2><strong>Add</strong> Faq </h2>
+                            <ul class="header-dropdown">
+                                <li class="remove">
+                                    <a role="button" class="boxs-close"><i class="zmdi zmdi-close"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="body">
+                            <div class="row clearfix">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <input type="text" name="title" class="form-control"
+                                            placeholder="Gallery Title">
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <input type="file" name="image" class="form-control">
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="row clearfix">
+                                <div class="col-sm-12">
+                                    <button type="submit" class="btn btn-primary btn-round">Submit</button>
+                                    <button type="submit" class="btn btn-default btn-round btn-simple">Cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
